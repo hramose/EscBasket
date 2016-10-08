23b4cef2bad71a6ef84a0528576b35c2f694f298
Travis CI <travis@travis-ci.org>
2016-10-08 00:58:12 +0000

    Deploy code docs to GitHub Pages Travis build: 31
    Commit: ea338e2e9fcf0d3db8e6fc1602d4976746f52103

--- html/search/all_63.js
+++ html/search/all_63.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['club',['Club',['../class_app_1_1_club.html',1,'App']]],
+  ['club_5finfo',['Club_info',['../class_app_1_1_club__info.html',1,'App']]],
   ['controller',['Controller',['../class_app_1_1_http_1_1_controllers_1_1_controller.html',1,'App::Http::Controllers']]],
   ['create',['create',['../class_app_1_1_http_1_1_controllers_1_1_auth_1_1_auth_controller.html#ab9f9163fd333871268ccbf1d5cc77b99',1,'App::Http::Controllers::Auth::AuthController']]],
   ['createapplication',['createApplication',['../class_test_case.html#a34b534b53804271b58c95106442a52f6',1,'TestCase']]]
--- html/search/classes_63.js
+++ html/search/classes_63.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['club',['Club',['../class_app_1_1_club.html',1,'App']]],
+  ['club_5finfo',['Club_info',['../class_app_1_1_club__info.html',1,'App']]],
   ['controller',['Controller',['../class_app_1_1_http_1_1_controllers_1_1_controller.html',1,'App::Http::Controllers']]]
 ];
