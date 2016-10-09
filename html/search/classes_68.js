05ae4e3703e6c36469cfcdbbf502d7655d2864d5
Travis CI <travis@travis-ci.org>
2016-10-09 01:43:42 +0000

    Deploy code docs to GitHub Pages Travis build: 45
    Commit: 3a427ef6b04ae96d47aa5d0cd352d2182a8893f4

--- html/search/classes_68.js
+++ html/search/classes_68.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['handler',['Handler',['../class_app_1_1_exceptions_1_1_handler.html',1,'App::Exceptions']]],
-  ['homepage',['Homepage',['../class_app_1_1_homepage.html',1,'App']]]
+  ['homepage',['Homepage',['../class_app_1_1_homepage.html',1,'App']]],
+  ['homepagecontroller',['HomepageController',['../class_app_1_1_http_1_1_controllers_1_1_homepage_controller.html',1,'App::Http::Controllers']]]
 ];
