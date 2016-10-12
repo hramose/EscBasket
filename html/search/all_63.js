198d5151f000e6404292812e8409b874db32ac66
Travis CI <travis@travis-ci.org>
2016-10-12 06:16:45 +0000

    Deploy code docs to GitHub Pages Travis build: 48
    Commit: c263c490f2e5a6a864b062c522dcca71976e16cf

--- html/search/all_63.js
+++ html/search/all_63.js
@@ -1,11 +1,13 @@
 var searchData=
 [
+  ['categoriesinhomepagetest',['categoriesInHomepageTest',['../classcategories_in_homepage_test.html',1,'']]],
   ['category',['Category',['../class_app_1_1_category.html',1,'App']]],
   ['club',['Club',['../class_app_1_1_club.html',1,'App']]],
   ['club_5finfo',['Club_info',['../class_app_1_1_club__info.html',1,'App']]],
   ['club_5finfoinhomepagetest',['club_infoInHomepageTest',['../classclub__info_in_homepage_test.html',1,'']]],
   ['clubinhomepagetest',['clubInHomepageTest',['../classclub_in_homepage_test.html',1,'']]],
   ['contact',['Contact',['../class_app_1_1_contact.html',1,'App']]],
+  ['contactinhomepagetest',['contactInHomepageTest',['../classcontact_in_homepage_test.html',1,'']]],
   ['controller',['Controller',['../class_app_1_1_http_1_1_controllers_1_1_controller.html',1,'App::Http::Controllers']]],
   ['create',['create',['../class_app_1_1_http_1_1_controllers_1_1_auth_1_1_auth_controller.html#ab9f9163fd333871268ccbf1d5cc77b99',1,'App::Http::Controllers::Auth::AuthController']]],
   ['createapplication',['createApplication',['../class_test_case.html#a34b534b53804271b58c95106442a52f6',1,'TestCase']]]
--- html/search/functions_74.js
+++ html/search/functions_74.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['testbasicexample',['testBasicExample',['../class_example_test.html#af29cc97005acacc3a49bde893ae509bb',1,'ExampleTest']]],
-  ['testexample',['testExample',['../classclub__info_in_homepage_test.html#ad7beb199de3f117c996164872b52e56f',1,'club_infoInHomepageTest\testExample()'],['../classclub_in_homepage_test.html#aaad5f178d7c02505789afbbf7449d1d2',1,'clubInHomepageTest\testExample()']]]
+  ['testcategoryinhomepage',['testCategoryInHomepage',['../classcategories_in_homepage_test.html#af2f51e60108be300d9cda350cab0156c',1,'categoriesInHomepageTest']]],
+  ['testclub_5finfoinhomepage',['testClub_infoInHomepage',['../classclub__info_in_homepage_test.html#aa2eb25c1db15b68fb10711d94f83e0f7',1,'club_infoInHomepageTest']]],
+  ['testclubinhomepage',['testClubInHomepage',['../classclub_in_homepage_test.html#a3d48b023c69deda17438584a52b885cc',1,'clubInHomepageTest']]],
+  ['testcontactinhomepage',['testContactInHomepage',['../classcontact_in_homepage_test.html#ae32fb9bbd7fb45bfb912453e5bfaedef',1,'contactInHomepageTest']]]
 ];
